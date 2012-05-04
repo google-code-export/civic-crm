1164e2b165e6dded2cbd2766bd1e278fa7758d9c
inghamn <inghamn@4e447b6a-6056-7046-2609-0fe1680c5616>
2012-05-04 19:47:48 +0000

    Created MySQL branch

---
+++ branches/mysql/crm/models/Location.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * @copyright 2011 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+class Location
+{
+	/**
+	 * Does different searches depending on the indexes of the $query
+	 *
+	 * @query['address'] Will do a search in CRM and a search for addresses in Master Address
+	 * @query['street'] Will do a search in CRM and a search for streets in Master Address
+	 * @query['text'] Will only do a search in CRM
+	 *
+	 * Addresses will be return as $array['location text'] = $flag
+	 * where $flag is true if returned by AddressService
+	 *
+	 * @param array $query
+	 * @return array
+	 */
+	public static function search($query)
+	{
+		$results = array();
+		$crm_query = '';
+
+		if (isset($query['address']) && $query['address']) {
+			$crm_query = $query['address'];
+		}
+		// Don't do a mongo search if they're doing a street search
+		// Mongo will return too many locations to be useful
+		elseif (isset($query['text']) && $query['text']) {
+			$crm_query = $query['text'];
+		}
+
+		if ($crm_query) {
+			$mongo = Database::getConnection();
+
+			$map = new MongoCode("function() {
+				var address_id='',city='';
+				if (this.address_id) {
+					address_id = this.address_id;
+				}
+				if (this.city) {
+					city = this.city;
+				}
+				emit(this.location,{count:1,address_id:address_id,city:city});
+			}");
+
+			$reduce = new MongoCode("function(key,values) {
+				var result = { count:0,address_id:'',city:'' };
+
+				for (var i in values) {
+					result.count += values[i].count;
+				}
+				if (values[i].address_id) {
+					result.address_id = values[i].address_id;
+				}
+				if (values[i].city) {
+					result.city = values[i].city;
+				}
+				return result;
+			}");
+
+			$q = $mongo->command(array(
+				'mapreduce'=>'tickets',
+				'map'=>$map,
+				'reduce'=>$reduce,
+				'query'=>array('location'=>new MongoRegex("/$crm_query/i")),
+				'out'=>array('inline'=>1)
+			));
+
+			foreach ($q['results'] as $location) {
+				$results[$location['_id']] = array(
+					'ticketCount'=>$location['value']['count'],
+					'address_id'=>$location['value']['address_id'],
+					'city'=>$location['value']['city'],
+					'source'=>'mongo'
+				);
+			}
+
+		}
+
+		if (isset($query['address']) && $query['address']) {
+			foreach (AddressService::searchAddresses($query['address']) as $location=>$data) {
+				if (!isset($results[$location])) {
+					$results[$location] = $data;
+				}
+				else {
+					$results[$location] = array_merge($results[$location],$data);
+				}
+				$results[$location]['source'] = 'Master Address';
+			}
+		}
+		elseif (isset($query['street']) && $query['street']) {
+			foreach (AddressService::searchStreets($query['street']) as $location=>$street_id) {
+				$results[$location]['address_id'] = $street_id;
+				$results[$location]['source'] = 'Master Address';
+			}
+		}
+
+		return $results;
+	}
+}
---
+++ branches/mysql/crm/models/PersonList.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * A collection class for Person objects
+ *
+ * @copyright 2011 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+class PersonList extends MongoResultIterator
+{
+	/**
+	 * @param array $fields
+	 */
+	public function __construct($fields=null)
+	{
+		parent::__construct();
+		if (is_array($fields)) {
+			$this->find($fields);
+		}
+	}
+
+	/**
+	 * Populates the collection, using strict matching of the requested fields
+	 *
+	 * @param array $fields
+	 * @param array $order
+	 */
+	public function find($fields=null,$order=array('lastname'=>1,'firstname'=>1))
+	{
+		$search = array();
+		if (count($fields)) {
+			foreach ($fields as $key=>$value) {
+				if ($value) {
+					switch ($key) {
+						case 'department':
+							$search['department._id'] = new MongoId($value);
+							break;
+						case 'phoneNumber':
+							$search['phone.number'] = $value;
+							break;
+						case 'phoneDeviceId':
+							$search['phone.device_id'] = $value;
+							break;
+						default:
+							$search[$key] = $value;
+					}
+				}
+			}
+		}
+		$this->runSearch($search,$order);
+	}
+
+	/**
+	 * Populates the collection, using regular expressions for matching
+	 *
+	 * @param array $fields
+	 * @param array $order
+	 */
+	public function search($fields=null,$order=array('lastname'=>1,'firstname'=>1))
+	{
+		$search = array();
+		if (isset($fields['query'])) {
+			$regex = new MongoRegex("/$fields[query]/i");
+			$search = array('$or'=>array(
+				array('firstname'=>$regex),
+				array('lastname'=>$regex),
+				array('email'=>$regex),
+				array('username'=>$regex)
+			));
+		}
+		elseif (count($fields)) {
+			foreach ($fields as $key=>$value) {
+				if ($key == 'phoneNumber')   $key = 'phone.number';
+				if ($key == 'phoneDeviceId') $key = 'phone.device_id';
+
+				if ($key == 'department') {
+					$search['department._id'] = new MongoId($value);
+				}
+				elseif (is_string($value)) {
+					$search[$key] = new MongoRegex("/$value/i");
+				}
+				elseif (is_array($value)) {
+					$search[$key] = $value;
+				}
+			}
+		}
+
+		$this->runSearch($search,$order);
+	}
+
+	private function runSearch($search=null,$order=null)
+	{
+		if (count($search)) {
+			$this->cursor = $this->mongo->people->find($search);
+		}
+		else {
+			$this->cursor = $this->mongo->people->find();
+		}
+		if ($order) {
+			$this->cursor->sort($order);
+		}
+	}
+
+	/**
+	 * Loads a single Person object for the row returned from ZendDbResultIterator
+	 *
+	 * @param array $key
+	 */
+	public function loadResult($data)
+	{
+		if ($data) {
+			return new Person($data);
+		}
+		else {
+			throw new Exception('resultIteratorSentEmptyData');
+		}
+	}
+}
